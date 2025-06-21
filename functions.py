2c9f249cb8c9b7871cd7f4eedf93ba74700698c1
chiranjeevi <jangilichiranjeevi2@gmail.com>
2025-06-21 18:36:07 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,15 @@
+name="chiru"
+age=20
+role="junior full stack developer"
+def hello():
+    print(name)
+    print(role)
+    print(age)
+hello()
+
+
+#2nd example
+def add(x,y):
+    return (x+y)
+result= add(2,5)
+print(result)
